4f464c97cec5afbcf2fd0802c562da4c1aaef50b
Amanyiraho <amanyiraho@gmail.com>
2024-12-10 12:37:34 +0300

    dec assessment

--- data-raw/questions_national.R
+++ data-raw/questions_national.R
@@ -6,7 +6,7 @@ library(stringr)
 
 questions_national <- read_xlsx("data-raw/Malaria Readiness Assessment_National.xlsx") |>
   select(name, label) |>
-  slice(-(1:18)) |>
+  slice(-(1:17)) |>
   filter(!(name  %in% c("starttime","endtime","today","readiness_assessment_checklist",
                         "malaria_intro","gps1", "level_supervision", "training","m_e",
                         "cold_chain_and_logistics", "advocacy_communication", "thank_you"))) |>
